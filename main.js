bc380507336e60af32f0aa785cd13e6a4f7f53ec
MGasataya <marescanog@gmail.com>
2021-07-19 07:40:04 +0800

    finished 3d background

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
       import { OrbitControls } from 'https://cdn.skypack.dev/three@0.130.1/examples/jsm/controls/OrbitControls.js';
       import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.130.1/examples/jsm/loaders/GLTFLoader.js';
 
+
+
 /*==============================*/
       /* Add scene Camera and Renderer */
       const scene = new THREE.Scene();
@@ -22,10 +24,6 @@
       renderer.setPixelRatio(devicePixelRatio);
 
  /*==============================*/
-      /* Add orbit controls */
-      // const controls = new OrbitControls( camera, renderer.domElement );
-
- /*==============================*/
       /* Add objects to the Scene */
 
       var isObjectsNotLoaded = true;
@@ -37,80 +35,183 @@
 
 
       /* GLTF LOADER */
-      const loader = new GLTFLoader();
-      var tex = false;
+      // const loader = new GLTFLoader();
+      // var tex = false;
 
-      loader.load( './images/FACE9.glb',  function ( gltf ) {
-        var object = gltf.scene;
+      // loader.load( './images/FACE9.glb',  function ( gltf ) {
+      //   var object = gltf.scene;
         
-        object.traverse((node)=>{
-          if(!node.isMesh) return;
-          node.material.wireframe = true;
-        })
-        var mat = new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: true } );
-        object.children[0].material = mat;
+      //   object.traverse((node)=>{
+      //     if(!node.isMesh) return;
+      //     node.material.wireframe = true;
+      //   })
+      //   var mat = new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: true } );
+      //   object.children[0].material = mat;
 
-        // console.log(object.children[0].geometry.attributes.position.array);
-        // console.log(object);
-        // console.log(mat);
-        scene.add( object );
+      //   // console.log(object.children[0].geometry.attributes.position.array);
+      //   // console.log(object);
+      //   // console.log(mat);
+      //   scene.add( object );
 
-      }, function ( xhr ) {
+      // }, function ( xhr ) {
 
-        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+      //   console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
     
-      }, function ( error ) {
+      // }, function ( error ) {
       
-        console.error( error );
+      //   console.error( error );
       
-      } );
+      // } );
 
       var object = scene.children;
-      console.log(object);
-      console.log(scene);
+      //console.log(object);
+      //console.log(scene);
 
       camera.position.z = 5;
 
-    const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
-    const planeMaterial = new THREE.MeshPhongMaterial({
-        color: 0xff0000,
-      side: THREE.DoubleSide
-    });
-    const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
-    //scene.add(planeMesh);
+
+
+    // const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
+    // const planeMaterial = new THREE.MeshPhongMaterial({
+    //     color: 0xff0000,
+    //   side: THREE.DoubleSide
+    // });
+    // const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+    // scene.add(planeMesh);
     
+
+
+
     const light = new THREE.DirectionalLight(
       0xffffff, 1);
     light.position.set(0,0,1);
     scene.add(light);
 
-      var notLoaded = true;
+    scene.add(new THREE.AmbientLight(0xffffff, 0.25));
 
-      var obj = new THREE.Object3D;
+    /* ===================================== */
+    /* ==== ADD Particle Objects Start ===== */
+    /* ===================================== */
+
+    function addPart() {
+      const geometry = new THREE.SphereGeometry(15, 4, 4);
+      const material = new THREE.MeshStandardMaterial({color: 0x1c1d63, wireframe: true, emissive: 0x676666});
+      const particle = new THREE.Mesh(geometry, material);
+      const[x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(800));
+      particle.position.set(x, y, z);
+      return particle;
+    }
+    var balls = new Array(100);  
+    for(var x = 0; x < balls.length-1; x++){
+      balls.fill( addPart() , x, x+1);
+      scene.add(balls[x]);
+    }
+
+;
+
+    /* ===================================== */
+    /* ==== ADD Particle Objects END ===== */
+    /* ===================================== */
+    
+    // copy array
+    // Need Original Position for motion
+    const ballsCopy = Array.from(balls);
+
+
+
+      // var notLoaded = true;
+
+      // var obj = new THREE.Object3D;
+
+ /*==============================*/
+      /* Add orbit controls */
+      // const controls = new OrbitControls( camera, renderer.domElement );
+
+
+
+
+    /* ===================================== */
+    /* ==== Move Camera on Scroll ===== */
+    /* ===================================== */
+
+var scrollyBoi = document.getElementById("scroll-container");
+
+function moveCamera(){
+  //console.log("SCOLLING!");
+  const currentScrollValue = scrollyBoi.scrollTop;
+
+  //console.log(currentScrollValue);
+
+  camera.position.z = currentScrollValue * -0.01;
+  camera.position.x = currentScrollValue * -0.01;
+  camera.position.y = currentScrollValue * -0.01;
+
+  camera.rotation.x = currentScrollValue * -0.0001;
+  camera.rotation.y = currentScrollValue * -0.00005;
+  camera.rotation.z = currentScrollValue * -0.00001;
+}
+
+
+scrollyBoi.onscroll = moveCamera;
+
+
+
+
+
 
 /*==============================*/
     /* ANIMATE */
     function animate() {
 
-      if(notLoaded){
-        if(scene.children[1]){
-          console.log("d");
-          notLoaded = false;
-          obj = scene;
 
-          console.log(obj.children[1].children[0].geometry.attributes.position.array);
+      // for(var x = 0; x < 1; x++){
+      //   //Get Match Cosine
+      //    const move = Math.cos(frame);
+      //   //Original Position array
+      //   console.log(ballsCopy[x].geometry.attributes.position.array.length);
+  
+  
+      //   for (var p = 0; p < ballsCopy[x].geometry.attributes.position.array.length; p++){
+      //     balls[x].geometry.attributes.position.array[p] = ballsCopy[x].geometry.attributes.position.array[p] + move;
+      //   }
+
+      // }
 
 
-        }
-      }
+
+      // if(notLoaded){
+      //   if(scene.children[1]){
+      //     console.log("d");
+      //     notLoaded = false;
+      //     obj = scene;
+
+      //     console.log(obj.children[1].children[0].geometry.attributes.position.array);
 
 
-      //controls.update();
+      //   }
+      // }
+
+
+
+
+    for(var x = 0; x < balls.length-1; x++){
+      const randomMizer = Math.random();
+      balls[x].rotation.x += -.01 * randomMizer;
+      balls[x].rotation.y += -.01 * randomMizer;
+      balls[x].rotation.y += -.01 * randomMizer;
+    }
+
+    
+
+
+
+
+      // controls.update();
 
       renderer.render(scene, camera);
       requestAnimationFrame(animate);
 
-      //console.log("sp");
+
     }
     animate();
 
@@ -120,5 +221,5 @@
     });
 
     gsap.to(camera.position,{
-      z: 3.5
+      z: 3.5,
     })
