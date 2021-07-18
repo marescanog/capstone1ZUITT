5d870694d62feaec943283f9de0d0c120df6e18c
MGasataya <marescanog@gmail.com>
2021-07-19 01:20:35 +0800

    binded dot movement to scroll bar

--- js/nav.js
+++ js/nav.js
@@ -1,16 +1,21 @@
-// var navStart = document.getElementById("nav-start");
-// var navProject = document.getElementById("nav-projects");
-// var navTools = document.getElementById("nav-tools");
-// var navContact = document.getElementById("nav-contact");
+var navStart = document.getElementById("nav-start");
+var navProject = document.getElementById("nav-projects");
+var navTools = document.getElementById("nav-tools");
+var navContact = document.getElementById("nav-contact");
+
+// console.log(navStart.getBoundingClientRect() );
+// console.log(navProject.getBoundingClientRect() );
+// console.log(navTools.getBoundingClientRect() );
+// console.log(navContact.getBoundingClientRect() );
 
 var navs = document.getElementsByClassName("c-svg-container-overlay");
 var majorDots = document.getElementsByClassName("c-major-dot");
+var movingDot = document.getElementById("moving-dot");
+var scrollContainer = document.getElementById("scroll-container");
 
-// console.log(navStart);
-// console.log(navProject);
-// console.log(navTools);
-// console.log(navContact);
-console.log(majorDots);
+
+var scrollElements = document.getElementsByClassName("c-scroll-snap-child");
+console.log(scrollElements[1].scrollTop);
 
 function setSelected(navType){
     for(var x = 0; x < navs.length; x++){
@@ -33,3 +38,55 @@ function setSelected(navType){
     }
     
 }
+
+
+var lastScrollTop = 0;
+
+var oldDotPosition = movingDot.getBoundingClientRect().top;
+console.log(oldDotPosition);
+/* Dot moves as ScrollBar moves */
+scrollContainer.addEventListener('scroll', () =>{
+    const currentScrollValue = scrollContainer.scrollTop;
+
+    // These only need to be computed once, move variables out (Only Chnage when window Resize is detected);
+    const totalScrollHeight= scrollContainer.scrollHeight - window.innerHeight;
+
+    // Get Current Scroll Percentage (The range result is 0 - 1)
+    const currentScrollPercentage = Math.round(((currentScrollValue/totalScrollHeight) + Number.EPSILON) * 100)/100;
+
+    // Get Range Conversion of the Nav Distance (Distance is 260)
+    const navBarAdd = currentScrollPercentage*260;
+
+
+//console.log(oldDotPosition + " + " + dotMoveValue + " =" );
+    // find out if scrolling up or down
+    if (currentScrollValue > lastScrollTop){
+        // Scrolling Down
+        movingDot.style.top =  (0 + navBarAdd) + "px" ;
+    } else {
+        // Scrolling Up
+        movingDot.style.top =  (0 + navBarAdd) + "px" ;
+    }
+    lastScrollTop = currentScrollValue;
+
+    
+});
+//TOP
+//movingDot.style.top = "-40px";
+//BOTTOM
+//movingDot.style.top = "220px";
+
+// @keyframes moving {
+//     0%{
+//         top:260px;
+//     }
+//     50%{
+//         top:530px;
+//     }
+//     100%{
+//         top:260px;
+//     }
+
+// 270 is total movement
+// divide totalScrollHeight by totalmovement
+// }
